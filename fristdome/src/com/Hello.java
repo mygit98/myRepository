09296b67bbdc68cd80ac01abf5b142685ef6b02f
ASUS <ASUS@DESKTOP-67UI3AG>
2020-04-12 04:11:15 +0800

    hhhhhhh

---
+++ fristdome/src/com/Hello.java
@@ -0,0 +1,12 @@
+package com;
+
+public class Hello {
+	
+	public static void main(String[] args) {
+		System.out.println(" ni 好我的新笔记本");
+		
+		
+		System.out.println("hello 我的世界  你好 我的未来");
+	}
+
+}
---
+++ springboot-helloworld/src/main/java/com/bjsxt/springboothelloworld/SpringbootHelloworldApplication.java
@@ -0,0 +1,14 @@
+package com.bjsxt.springboothelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@EnableEurekaServer
+@SpringBootApplication
+public class SpringbootHelloworldApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHelloworldApplication.class, args);
+	}
+}
